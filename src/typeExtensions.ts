455c9f666e70556ec660566cd9d66b60216dd6ff
jphillipsCrestron <jphillips@crestron.com>
2023-12-09 17:19:32 -0600

    Removed unnecessary bridge methods

--- src/typeExtensions.ts
+++ src/typeExtensions.ts
@@ -1,18 +1,8 @@
 import * as CrComLib from '@crestron/ch5-crcomlib';
-import { 
-  bridgeReceiveBooleanFromNative, 
-  bridgeReceiveIntegerFromNative, 
-  bridgeReceiveObjectFromNative, 
-  bridgeReceiveStringFromNative,
- } from '@crestron/ch5-crcomlib';
 
 declare global {
   interface Window {
     CrComLib: typeof CrComLib;
-    bridgeReceiveBooleanFromNative: typeof bridgeReceiveBooleanFromNative;
-    bridgeReceiveIntegerFromNative: typeof bridgeReceiveIntegerFromNative;
-    bridgeReceiveObjectFromNative: typeof bridgeReceiveObjectFromNative;
-    bridgeReceiveStringFromNative: typeof bridgeReceiveStringFromNative;
   }
 }
 
@@ -22,9 +12,3 @@ interface CrComLibType {
 
 // Assign to window object
 window.CrComLib = (CrComLib as unknown as CrComLibType).CrComLib;
-
-// Assign the bridge receive methods to the window object
-window.bridgeReceiveBooleanFromNative = bridgeReceiveBooleanFromNative;
-window.bridgeReceiveIntegerFromNative = bridgeReceiveIntegerFromNative;
-window.bridgeReceiveStringFromNative = bridgeReceiveStringFromNative;
-window.bridgeReceiveObjectFromNative = bridgeReceiveObjectFromNative;
