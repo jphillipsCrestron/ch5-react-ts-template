843b7e06cf70c50f4ac3660285a014b152a7d90c
jphillipsCrestron <jphillips@crestron.com>
2023-12-09 16:56:32 -0600

    Cleanup event listeners on component unmount

--- src/hooks/useWebXPanel.ts
+++ src/hooks/useWebXPanel.ts
@@ -7,45 +7,64 @@ const useWebXPanel = (host: string, ipId: string, roomId?: string) => {
 
   useEffect(() => {
     const { WebXPanel, isActive, WebXPanelEvents, WebXPanelConfigParams } = getWebXPanel(!runsInContainerApp());
-
+  
     setIsActive(isActive);
-
-    const config: Partial<typeof WebXPanelConfigParams> = { host, ipId, roomId, };
-
+  
+    const config: Partial<typeof WebXPanelConfigParams> = { host, ipId, roomId };
+  
     if (isActive) {
-        console.log("Initializing XPanel with config: " + JSON.stringify(config));
-        WebXPanel.initialize(config);
-
-        window.addEventListener(WebXPanelEvents.CONNECT_WS, () => {
-            console.log("WebXPanel websocket connection success");
-        });
-        
-        window.addEventListener(WebXPanelEvents.ERROR_WS, ({ detail }: any) => {
-            console.log(`WebXPanel websocket connection error: ${JSON.stringify(detail)}`);
-        });
-        
-        window.addEventListener(WebXPanelEvents.CONNECT_CIP, () => {
-            console.log("WebXPanel CIP connection success");
-        });
-        
-        window.addEventListener(WebXPanelEvents.AUTHENTICATION_FAILED, ({ detail }: any) => {
-            console.log(`WebXPanel authentication failed: ${JSON.stringify(detail)}`);
-        });
-        
-        window.addEventListener(WebXPanelEvents.NOT_AUTHORIZED, ({ detail }: any) => {
-            console.log(`WebXPanel not authorized: ${JSON.stringify(detail)}`);
-            window.location = detail.redirectTo;
-        });
-        
-        window.addEventListener(WebXPanelEvents.DISCONNECT_WS, ({ detail }: any) => {
-            console.log(`WebXPanel websocket connection lost: ${JSON.stringify(detail)}`);
-        });
-        
-        window.addEventListener(WebXPanelEvents.DISCONNECT_CIP, ({ detail }: any) => {
-            console.log(`WebXPanel CIP connection lost: ${JSON.stringify(detail)}`);
-        });
+      console.log("Initializing XPanel with config: " + JSON.stringify(config));
+      WebXPanel.initialize(config);
+  
+      const connectWsListener = () => {
+        console.log("WebXPanel websocket connection success");
+      };
+  
+      const errorWsListener = ({ detail }: any) => {
+        console.log(`WebXPanel websocket connection error: ${JSON.stringify(detail)}`);
+      };
+  
+      const connectCipListener = () => {
+        console.log("WebXPanel CIP connection success");
+      };
+  
+      const authenticationFailedListener = ({ detail }: any) => {
+        console.log(`WebXPanel authentication failed: ${JSON.stringify(detail)}`);
+      };
+  
+      const notAuthorizedListener = ({ detail }: any) => {
+        console.log(`WebXPanel not authorized: ${JSON.stringify(detail)}`);
+        window.location = detail.redirectTo;
+      };
+  
+      const disconnectWsListener = ({ detail }: any) => {
+        console.log(`WebXPanel websocket connection lost: ${JSON.stringify(detail)}`);
+      };
+  
+      const disconnectCipListener = ({ detail }: any) => {
+        console.log(`WebXPanel CIP connection lost: ${JSON.stringify(detail)}`);
+      };
+  
+      // Adding event listeners
+      window.addEventListener(WebXPanelEvents.CONNECT_WS, connectWsListener);
+      window.addEventListener(WebXPanelEvents.ERROR_WS, errorWsListener);
+      window.addEventListener(WebXPanelEvents.CONNECT_CIP, connectCipListener);
+      window.addEventListener(WebXPanelEvents.AUTHENTICATION_FAILED, authenticationFailedListener);
+      window.addEventListener(WebXPanelEvents.NOT_AUTHORIZED, notAuthorizedListener);
+      window.addEventListener(WebXPanelEvents.DISCONNECT_WS, disconnectWsListener);
+      window.addEventListener(WebXPanelEvents.DISCONNECT_CIP, disconnectCipListener);
+  
+      // Cleanup function
+      return () => {
+        window.removeEventListener(WebXPanelEvents.CONNECT_WS, connectWsListener);
+        window.removeEventListener(WebXPanelEvents.ERROR_WS, errorWsListener);
+        window.removeEventListener(WebXPanelEvents.CONNECT_CIP, connectCipListener);
+        window.removeEventListener(WebXPanelEvents.AUTHENTICATION_FAILED, authenticationFailedListener);
+        window.removeEventListener(WebXPanelEvents.NOT_AUTHORIZED, notAuthorizedListener);
+        window.removeEventListener(WebXPanelEvents.DISCONNECT_WS, disconnectWsListener);
+        window.removeEventListener(WebXPanelEvents.DISCONNECT_CIP, disconnectCipListener);
+      };
     }
-
   }, [host, ipId, roomId]);
 
   return { isActive };
