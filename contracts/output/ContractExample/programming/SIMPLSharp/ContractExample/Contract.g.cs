f52bdd050bdb7d63bdeb8e70510c624493e2f346
jphillipsCrestron <jphillips@crestron.com>
2025-02-08 20:15:21 -0600

    feat [CrComLib]: Add contract example

---
+++ contracts/output/ContractExample/programming/SIMPLSharp/ContractExample/Contract.g.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Crestron.SimplSharpPro.DeviceSupport;
+using Crestron.SimplSharpPro;
+
+namespace ContractExample
+{
+    /// <summary>
+    /// Common Interface for Root Contracts.
+    /// </summary>
+    public interface IContract
+    {
+        object UserObject { get; set; }
+        void AddDevice(BasicTriListWithSmartObject device);
+        void RemoveDevice(BasicTriListWithSmartObject device);
+    }
+
+    public class Contract : IContract, IDisposable
+    {
+        #region Components
+
+        private ComponentMediator ComponentMediator { get; set; }
+
+        public ContractExample.IHomePage HomePage { get { return (ContractExample.IHomePage)InternalHomePage; } }
+        private ContractExample.HomePage InternalHomePage { get; set; }
+
+        #endregion
+
+        #region Construction and Initialization
+
+        public Contract()
+            : this(new List<BasicTriListWithSmartObject>().ToArray())
+        {
+        }
+
+        public Contract(BasicTriListWithSmartObject device)
+            : this(new [] { device })
+        {
+        }
+
+        public Contract(BasicTriListWithSmartObject[] devices)
+        {
+            if (devices == null)
+                throw new ArgumentNullException("Devices is null");
+
+            ComponentMediator = new ComponentMediator();
+
+            InternalHomePage = new ContractExample.HomePage(ComponentMediator, 1);
+
+            for (int index = 0; index < devices.Length; index++)
+            {
+                AddDevice(devices[index]);
+            }
+        }
+
+        #endregion
+
+        #region Standard Contract Members
+
+        public object UserObject { get; set; }
+
+        public void AddDevice(BasicTriListWithSmartObject device)
+        {
+            InternalHomePage.AddDevice(device);
+        }
+
+        public void RemoveDevice(BasicTriListWithSmartObject device)
+        {
+            InternalHomePage.RemoveDevice(device);
+        }
+
+        #endregion
+
+        #region IDisposable
+
+        public bool IsDisposed { get; set; }
+
+        public void Dispose()
+        {
+            if (IsDisposed)
+                return;
+
+            IsDisposed = true;
+
+            InternalHomePage.Dispose();
+            ComponentMediator.Dispose(); 
+        }
+
+        #endregion
+
+    }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,9 @@ function App() {
   const [digitalState, setDigitalState] = useState(false);
   const [analogState, setAnalogState] = useState(0);
   const [serialState, setSerialState] = useState("");
+  const [digitalContractState, setDigitalContractState] = useState(false);
+  const [analogContractState, setAnalogContractState] = useState(0);
+  const [serialContractState, setSerialContractState] = useState("");
 
   const webXPanelConfig = useMemo(() => ({
     ipId: '0x03',
@@ -25,18 +28,27 @@ function App() {
 
   useEffect(() => {
     // Listen for digital, analog, and serial joins 1 from the control system.
-    // d1Id, a1Id, and s1Id are the subscription IDs for each join, they do not
-    // carry any values and are only used to unsubscribe from the join when the
-    // component unmounts
+    // d1Id, a1Id, and s1Id are the subscription IDs for each join, they are 
+    // only used to unsubscribe from the join when the component unmounts
     const d1Id = window.CrComLib.subscribeState('b', '1', (value: boolean) => setDigitalState(value));
     const a1Id = window.CrComLib.subscribeState('n', '1', (value: number) => setAnalogState(value));
     const s1Id = window.CrComLib.subscribeState('s', '1', (value: string) => setSerialState(value));
+
+    // Contracts
+    const dc1Id = window.CrComLib.subscribeState('b', 'HomePage.DigitalState', (value: boolean) => setDigitalContractState(value));
+    const ac1Id = window.CrComLib.subscribeState('n', 'HomePage.AnalogState', (value: number) => setAnalogContractState(value));
+    const sc1Id = window.CrComLib.subscribeState('s', 'HomePage.StringState', (value: string) => setSerialContractState(value));
 
     return () => {
       // Unsubscribe from digital, analog, and serial joins 1 when component unmounts
       window.CrComLib.unsubscribeState('b', '1', d1Id);
       window.CrComLib.unsubscribeState('n', '1', a1Id);
       window.CrComLib.unsubscribeState('s', '1', s1Id);
+
+      // Contracts
+      window.CrComLib.unsubscribeState('b', 'HomePage.DigitalState', dc1Id);
+      window.CrComLib.unsubscribeState('n', 'HomePage.AnalogState', ac1Id);
+      window.CrComLib.unsubscribeState('s', 'HomePage.StringState', sc1Id);
     }
   }, []);
 
@@ -45,20 +57,44 @@ function App() {
   const sendAnalog = (value: number) => window.CrComLib.publishEvent('n', '1', value);
   const sendSerial = (value: string) => window.CrComLib.publishEvent('s', '1', value);
 
+  // Contracts
+  const sendDigitalContract = (value: boolean) => window.CrComLib.publishEvent('b', 'HomePage.DigitalEvent', value);
+  const sendAnalogContract = (value: number) => window.CrComLib.publishEvent('n', 'HomePage.AnalogEvent', value);
+  const sendSerialContract = (value: string) => window.CrComLib.publishEvent('s', 'HomePage.StringEvent', value);
+
   return (
-    <div id="controlGroupWrapper">
-      <div className="controlGroup">
-        <button id="sendDigitalButton" className="btn" onClick={() => sendDigital(!digitalState)}>Toggle Digital</button>
-        <p id="currentDigitalValue">{digitalState.toString()}</p>
+    <>
+      {/* Joins */}
+      <p style={{ color: 'white' }}>Joins</p>
+      <div id="controlGroupWrapper">
+        <div className="controlGroup">
+          <button id="sendDigitalButton" className="btn" onClick={() => sendDigital(!digitalState)}>Toggle Digital</button>
+          <p id="currentDigitalValue">{digitalState.toString()}</p>
+        </div>
+        <div className="controlGroup">
+            <p id="currentAnalogValue">{analogState}</p>
+            <input type="range" min="0" max="65535" value={analogState} placeholder="32767" id="analogSlider" onChange={(e) => sendAnalog(Number(e.target.value))} />
+        </div>
+        <div className="controlGroup">
+            <input type="text" name="Data" id="currentSerialValue" placeholder="Placeholder" value={serialState} onChange={(e) => sendSerial(e.target.value)} />
+        </div>
       </div>
-      <div className="controlGroup">
-          <p id="currentAnalogValue">{analogState}</p>
-          <input type="range" min="0" max="65535" value={analogState} placeholder="32767" id="analogSlider" onChange={(e) => sendAnalog(Number(e.target.value))} />
+      {/* Contracts */}
+      <p style={{ color: 'white' }}>Contracts</p>
+      <div id="controlGroupWrapper">
+        <div className="controlGroup">
+          <button id="sendDigitalButton" className="btn" onClick={() => sendDigitalContract(!digitalContractState)}>Toggle Digital</button>
+          <p id="currentDigitalValue">{digitalContractState.toString()}</p>
+        </div>
+        <div className="controlGroup">
+            <p id="currentAnalogValue">{analogContractState}</p>
+            <input type="range" min="0" max="65535" value={analogContractState} placeholder="32767" id="analogSlider" onChange={(e) => sendAnalogContract(Number(e.target.value))} />
+        </div>
+        <div className="controlGroup">
+            <input type="text" name="Data" id="currentSerialValue" placeholder="Placeholder" value={serialContractState} onChange={(e) => sendSerialContract(e.target.value)} />
+        </div>
       </div>
-      <div className="controlGroup">
-          <input type="text" name="Data" id="currentSerialValue" placeholder="Placeholder" value={serialState} onChange={(e) => sendSerial(e.target.value)} />
-      </div>
-    </div>
+    </>
   )
 }
 
